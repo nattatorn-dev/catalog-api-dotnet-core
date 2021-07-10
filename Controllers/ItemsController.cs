5736168a324d03cc65153d8d9d5e58e11426923b
Nattatorn Yucharoen <nattatorn.yuch@Nattatorns-MacBook-Pro.local>
2021-07-10 19:09:34 +0700

    example dotnet core

---
+++ Controllers/ItemsController.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using Catalog.Entities;
+using Catalog.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.Controllers
+{
+  [ApiController]
+  [Route("items")]
+  public class ItemsController : ControllerBase
+  {
+    private readonly InMemItemsRepository repository;
+
+    public ItemsController()
+    {
+      repository = new InMemItemsRepository();
+    }
+
+    [HttpGet]
+    public IEnumerable<Item> GetItems() 
+    {
+      var items = repository.GetItems();
+      return items;
+    }
+
+    [HttpGet("{id}")]
+    public Item GetItem(Guid id)
+    {
+      var item = repository.GetItem(id);
+      return item;
+    }
+  }
+}
