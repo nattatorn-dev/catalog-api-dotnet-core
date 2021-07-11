b09946b52014fbae472e647168d344c3e6134acb
Nattatorn Yucharoen <nattatorn.yuch@Nattatorns-MacBook-Pro.local>
2021-07-11 13:27:01 +0700

    add health check

--- README.md
+++ README.md
@@ -16,5 +16,63 @@ docker run -d --rm --name mongo -p 27017:27017 -v mongodbdata:/data/db \
 - MVC
 - DTO
 - Integrate MongoDB
+
+```sh
+dotnet add package MongoDB.Driver
+```
+
 - Sync/Async Task Operation
 - Secret Management
+
+```sh
+dotnet user-secrets init
+dotnet user-secrets set MongoDbSettings:Password password
+```
+
+- Health checks liveness/readiness
+
+```sh
+dotnet add package AspNetCore.HealthChecks.MongoDb
+```
+
+- Custom health check
+
+Unhealthy
+
+```sh
+docker pause mongo
+```
+
+```json
+{
+  "status": "Unhealthy",
+  "checks": [
+    {
+        "name": "mongodb",
+        "status": "Unhealthy",
+        "exception": "The operation was canceled.",
+        "duration": "00:00:03.0196882"
+    }
+  ]
+}
+```
+
+Healthy
+
+```sh
+docker unpause mongo
+```
+
+```json
+{
+  "status": "Healthy",
+  "checks": [
+    {
+        "name": "mongodb",
+        "status": "Healthy",
+        "exception": "none",
+        "duration": "00:00:00.0019597"
+    }
+  ]
+}
+```
--- Startup.cs
+++ Startup.cs
@@ -1,11 +1,15 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mime;
+using System.Text.Json;
 using System.Threading.Tasks;
 using Catalog.Repositories;
 using Catalog.Settings;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
@@ -34,22 +38,30 @@ namespace Catalog
     {
       BsonSerializer.RegisterSerializer(new GuidSerializer(BsonType.String));
       BsonSerializer.RegisterSerializer(new DateTimeOffsetSerializer(BsonType.String));
+      var mongoDbsettings = Configuration.GetSection(nameof(MongoDbSettings)).Get<MongoDbSettings>();
 
       services.AddSingleton<IMongoClient>(ServiceProvider =>
       {
-        var settings = Configuration.GetSection(nameof(MongoDbSettings)).Get<MongoDbSettings>();
-        return new MongoClient(settings.ConnectionString);
+        return new MongoClient(mongoDbsettings.ConnectionString);
       });
 
       services.AddSingleton<IItemsRepository, MongoDbItemsRepository>();
 
-      services.AddControllers(options => {
+      services.AddControllers(options =>
+      {
         options.SuppressAsyncSuffixInActionNames = false;
       });
       services.AddSwaggerGen(c =>
       {
         c.SwaggerDoc("v1", new OpenApiInfo { Title = "Catalog", Version = "v1" });
       });
+
+      services.AddHealthChecks()
+        .AddMongoDb(
+          mongoDbsettings.ConnectionString,
+          name: "mongodb",
+          timeout: TimeSpan.FromSeconds(3),
+          tags: new[] { "ready" });
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -71,6 +83,31 @@ namespace Catalog
       app.UseEndpoints(endpoints =>
       {
         endpoints.MapControllers();
+
+        endpoints.MapHealthChecks("/health/ready", new HealthCheckOptions{
+          Predicate = (check) => check.Tags.Contains("ready"),
+          ResponseWriter = async(context, report) =>
+          {
+            var result = JsonSerializer.Serialize(
+              new{
+                status = report.Status.ToString(),
+                checks = report.Entries.Select(entry => new {
+                  name = entry.Key,
+                  status = entry.Value.Status.ToString(),
+                  exception = entry.Value.Exception != null ? entry.Value.Exception.Message : "none",
+                  duration = entry.Value.Duration.ToString()
+                })
+              }
+            );
+
+            context.Response.ContentType = MediaTypeNames.Application.Json;
+            await context.Response.WriteAsync(result);
+          }
+        });
+
+        endpoints.MapHealthChecks("/health/live", new HealthCheckOptions{
+          Predicate = (_) => false
+        });
       });
     }
   }
